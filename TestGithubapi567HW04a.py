c8cca67cfd9ceb1bf6f36303778bbacc7a229671
Deeptidevi Agrawal <agrawal.deeptidevi@gmail.com>
2021-03-17 01:10:21 -0400

    HW 05a

--- TestGithubapi567HW04a.py
+++ TestGithubapi567HW04a.py
@@ -10,6 +10,8 @@ from Githubapi567HW04a import getGithubRepoURL, getGithubRepoCommitsURL, getGith
 import unittest
 import json
 import time
+from unittest import mock
+from unittest.mock import Mock, patch
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
@@ -18,79 +20,221 @@ class TestGithubapi567HW04a(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def test_GetGithubRepoURL_GettingFormedCorrectlyUsingGithubID(self): 
-        time.sleep(5)
         self.assertEqual(getGithubRepoURL('richkempinski'),'https://api.github.com/users/richkempinski/repos')
 
     def test_GetGithubRepoURL_ThrowingExceptionWhenGithubIDNotProvided(self): 
-        time.sleep(5)
         self.assertRaises(Exception,'GithubID not provided' ,getGithubRepoURL, None)
 
     def test_GetGithubRepoCommitsURL_GettingFormedCorrectlyUsingGithubIDAndRepo(self): 
-        time.sleep(5)
         self.assertEqual(getGithubRepoCommitsURL('richkempinski','hellogitworld'),'https://api.github.com/repos/richkempinski/hellogitworld/commits')
 
     def test_GetGithubRepoCommitsURL_ThrowingExceptionWhenGithubIDNotProvided(self): 
-        time.sleep(5)
         self.assertRaises(Exception,'Either githubID or githubRepo not provided' ,getGithubRepoCommitsURL, None, 'hellogitworld')
 
     def test_GetGithubRepoCommitsURL_ThrowingExceptionWhenGithubRepoNotProvided(self): 
-        time.sleep(5)
         self.assertRaises(Exception,'Either githubID or githubRepo not provided' ,getGithubRepoCommitsURL, 'richkempinski', None)
 
-    def test_GetGithubResponse_ThrowingExceptionWhenIncorrectURLProvided(self): 
-        time.sleep(5)
-        self.assertRaises(Exception,'Github user not found' ,getGithubResponse, 'https://api.github.com/users//repos')
-
     def test_GetGithubResponse_returningResponseOfTypeJSON(self): 
-        time.sleep(5)
-        self.assertIsInstance(getGithubResponse('https://api.github.com/users/richkempinski/repos'), list)
+        mock_get_patcher = patch('Githubapi567HW04a.requests.get')
+        mock_get = mock_get_patcher.start()
+        githubResponse = [
+        {
+        "name": "csp"
+        },
+        {
+        "name": "hellogitworld"
+        },
+        {
+        "name": "helloworld"
+        },
+        {
+        "name": "Mocks"
+        },
+        {
+        "name": "Project1"
+        },
+        {
+        "name": "richkempinski.github.io"
+        },
+        {
+        "name": "threads-of-life"
+        },
+        {
+        "name": "try_nbdev"
+        },
+        {
+        "name": "try_nbdev2"
+        }
+        ]
+        #self.mock_get.return_value = Mock()
+        mock_get.return_value.json.return_value = githubResponse
+        mock_get.return_value.ok = True
+        mock_get.return_value.status_code = 200
+        response = getGithubResponse('https://api.github.com/users/richkempinski/repos')
+        mock_get_patcher.stop()
+        self.assertIsInstance(response, list)
 
     def test_GetGithubResponse_SizeIsGreatherThan0(self): 
-        time.sleep(5)
-        self.assertNotEqual(len(getGithubResponse('https://api.github.com/users/richkempinski/repos')), 0)
-        self.assertIsNotNone(getGithubResponse('https://api.github.com/users/richkempinski/repos'))
+        mock_get_patcher = patch('Githubapi567HW04a.requests.get')
+        mock_get = mock_get_patcher.start()
+        githubResponse = [
+        {
+        "name": "csp"
+        },
+        {
+        "name": "hellogitworld"
+        },
+        {
+        "name": "helloworld"
+        },
+        {
+        "name": "Mocks"
+        },
+        {
+        "name": "Project1"
+        },
+        {
+        "name": "richkempinski.github.io"
+        },
+        {
+        "name": "threads-of-life"
+        },
+        {
+        "name": "try_nbdev"
+        },
+        {
+        "name": "try_nbdev2"
+        }
+        ]
+        #self.mock_get.return_value = Mock()
+        mock_get.return_value.json.return_value = githubResponse
+        mock_get.return_value.ok = True
+        mock_get.return_value.status_code = 200
+        response = getGithubResponse('https://api.github.com/users/richkempinski/repos')
+        mock_get_patcher.stop()
+        self.assertNotEqual(len(response), 0)
+        self.assertIsNotNone(response)
 
     def test_GetGithubRepos_SizeIsGreatherThan0(self):
-        time.sleep(5)
-        self.assertNotEqual(len(getGithubRepos('richkempinski')), 0)
-        self.assertIsNotNone(getGithubRepos('richkempinski'))
+        mock_get_patcher = patch('Githubapi567HW04a.requests.get')
+        mock_get = mock_get_patcher.start()
+        githubResponse = [
+        {
+        "name": "csp"
+        },
+        {
+        "name": "hellogitworld"
+        },
+        {
+        "name": "helloworld"
+        },
+        {
+        "name": "Mocks"
+        },
+        {
+        "name": "Project1"
+        },
+        {
+        "name": "richkempinski.github.io"
+        },
+        {
+        "name": "threads-of-life"
+        },
+        {
+        "name": "try_nbdev"
+        },
+        {
+        "name": "try_nbdev2"
+        }
+        ]
+        #self.mock_get.return_value = Mock()
+        mock_get.return_value.json.return_value = githubResponse
+        mock_get.return_value.ok = True
+        mock_get.return_value.status_code = 200
+        response = getGithubRepos('richkempinski')
+        mock_get_patcher.stop()
+        self.assertNotEqual(len(response), 0)
+        self.assertIsNotNone(response)
 
     def test_GetGithubRepos_returningResponseOfTypeList(self): 
-        time.sleep(5)
-        self.assertIsInstance(getGithubRepos('richkempinski'), list)
+        mock_get_patcher = patch('Githubapi567HW04a.requests.get')
+        mock_get = mock_get_patcher.start()
+        githubResponse = [
+        {
+        "name": "csp"
+        },
+        {
+        "name": "hellogitworld"
+        },
+        {
+        "name": "helloworld"
+        },
+        {
+        "name": "Mocks"
+        },
+        {
+        "name": "Project1"
+        },
+        {
+        "name": "richkempinski.github.io"
+        },
+        {
+        "name": "threads-of-life"
+        },
+        {
+        "name": "try_nbdev"
+        },
+        {
+        "name": "try_nbdev2"
+        }
+        ]
+        #self.mock_get.return_value = Mock()
+        mock_get.return_value.json.return_value = githubResponse
+        mock_get.return_value.ok = True
+        mock_get.return_value.status_code = 200
+        response = getGithubRepos('richkempinski')
+        mock_get_patcher.stop()
+        self.assertIsInstance(response, list)
 
     def test_GetGithubRepos_ThrowingExceptionWhenIncorrectUserProvided(self): 
-        time.sleep(5)
         self.assertRaises(Exception,'Github user not found' ,getGithubRepos, None)
 
     def test_GetGithubRepos_SizeIs0WhenRepoNotFound(self):
-        time.sleep(5)
-        self.assertEqual(len(getGithubRepos('sdfsdf')), 0)
+        mock_get_patcher = patch('Githubapi567HW04a.requests.get')
+        mock_get = mock_get_patcher.start()
+        githubResponse = []
+        #self.mock_get.return_value = Mock()
+        mock_get.return_value.json.return_value = githubResponse
+        mock_get.return_value.ok = True
+        mock_get.return_value.status_code = 200
+        response = getGithubRepos('sdfsdf')
+        mock_get_patcher.stop()
+        self.assertEqual(len(response), 0)
 
     def test_GetGithubRepoCommits_SizeIsGreatherThan0(self):
-        time.sleep(5)
-        self.assertNotEqual(getGithubRepoCommits('richkempinski','hellogitworld'), 0)
-        self.assertIsNotNone(getGithubRepoCommits('richkempinski','hellogitworld'))
+        mock_get_patcher = patch('Githubapi567HW04a.requests.get')
+        mock_get = mock_get_patcher.start()
+        githubResponse = [{},{},{},{}]
+        #self.mock_get.return_value = Mock()
+        mock_get.return_value.json.return_value = githubResponse
+        mock_get.return_value.ok = True
+        mock_get.return_value.status_code = 200
+        response = getGithubRepoCommits('richkempinski','hellogitworld')
+        mock_get_patcher.stop()
+        self.assertNotEqual(response, 0)
+        self.assertIsNotNone(response)
 
     def test_GetGithubRepoCommits_returningResponseOfTypeInt(self): 
-        time.sleep(5)
-        self.assertIsInstance(getGithubRepoCommits('richkempinski','hellogitworld'), int)
-
-    def test_GetGithubRepoCommits_ThrowingExceptionWhenIncorrectUserRepoProvided(self): 
-        time.sleep(5)
-        self.assertRaises(Exception,'Github user not found' ,getGithubRepoCommits, None, None)
-
-    def test_getGitHubReposAndCommitsForAUser_returningResponseOfTypeList(self): 
-        time.sleep(5)
-        self.assertIsInstance(getGitHubReposAndCommitsForAUser('richkempinski'), list)
-
-    def test_getGitHubReposAndCommitsForAUser_ThrowingExceptionWhenIncorrectUserRepoProvided(self):
-        time.sleep(5)
-        self.assertRaises(Exception,'Github user not found' ,getGitHubReposAndCommitsForAUser, None)
-
-    def test_getGitHubReposAndCommitsForAUser_SizeIs0WhenRepoNotFound(self):
-        time.sleep(5)
-        self.assertEqual(len(getGitHubReposAndCommitsForAUser('sdfsdf')), 0)
+        mock_get_patcher = patch('Githubapi567HW04a.requests.get')
+        mock_get = mock_get_patcher.start()
+        githubResponse = [{},{},{},{}]
+        #self.mock_get.return_value = Mock()
+        mock_get.return_value.json.return_value = githubResponse
+        mock_get.return_value.ok = True
+        mock_get.return_value.status_code = 200
+        response = getGithubRepoCommits('richkempinski','hellogitworld')
+        mock_get_patcher.stop()
+        self.assertIsInstance(response, int)
 
 if __name__ == '__main__':
     print('Running unit tests')
